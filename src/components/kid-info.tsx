3c0de46ff7177e6dc206ba84258e5bb7a1e0d36e
Jonathan White <white.drgn82@gmail.com>
2020-09-19 15:40:07 -0600

    Condensed smaller components and changed checkboxes to buttons

--- src/components/kid-info.tsx
+++ src/components/kid-info.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, ChangeEvent } from "react";
+import { ButtonGroup, Button } from "react-bootstrap";
 import { Kid } from "../types";
-import { ChoreList } from "./chore_list";
 import { UpdateKidChoreDataContext, KidChoreDataContext } from "../contexts";
 
 interface KidInfoProps {
@@ -10,14 +10,15 @@ interface KidInfoProps {
 export const KidInfo: FunctionComponent<KidInfoProps> = ({ kid }) => {
   const data = React.useContext(KidChoreDataContext);
   const setData = React.useContext(UpdateKidChoreDataContext);
-  const handleCheckedChanged = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     let choreIndex = parseInt(e.currentTarget.id.split("-")[1], 10);
     let updateData = [...data];
     let kidIndex: number;
     data.forEach((k, i) => {
       if (k.name === kid.name) kidIndex = i;
     });
-    updateData[kidIndex].chores[choreIndex].completed = e.currentTarget.checked;
+    updateData[kidIndex].chores[choreIndex].completed = !kid.chores[choreIndex]
+      .completed;
     setData(updateData);
   };
   return kid ? (
@@ -25,10 +26,31 @@ export const KidInfo: FunctionComponent<KidInfoProps> = ({ kid }) => {
       <h1>
         {kid.name} - {kid.points} points
       </h1>
-      <ChoreList
-        chores={kid.chores}
-        handleCheckedChanged={handleCheckedChanged}
-      />
+      <ButtonGroup vertical>
+        {kid.chores.map(({ completed, name }, i) => {
+          return (
+            <Button
+              style={
+                completed
+                  ? {
+                      textDecorationLine: "line-through",
+                      textDecorationStyle: "solid",
+                    }
+                  : null
+              }
+              variant={completed ? "success" : "secondary"}
+              size="lg"
+              className="m-1"
+              key={i}
+              value={i}
+              id={"chore-" + i}
+              onClick={handleClick}
+            >
+              {name}
+            </Button>
+          );
+        })}
+      </ButtonGroup>
     </div>
   ) : (
     <div>No Kid Data</div>
